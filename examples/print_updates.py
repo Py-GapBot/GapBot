f91955e4a974fbffa99fee2704a573f17ddb161f
MrMahdi313 <m.m.z.m12363@gmail.com>
2019-06-08 12:41:40 -0400

    upload print_updates example

---
+++ examples/print_updates.py
@@ -0,0 +1,14 @@
+from GapBot import Gap
+
+app = Gap(
+    bot_token='6327819b0a5a3401bd11f616b9f5d2754e24162610b2a39f94fd299485ff3175'
+)
+
+
+@app.on_update
+def _update_handler(bot: Gap, update):
+    print('{}'.format(update))
+
+
+if __name__ == '__main__':
+    app.run()
