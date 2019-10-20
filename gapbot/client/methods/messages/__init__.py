28880d907c70f363e258c0d3e2049185da716887
MrMahdi313 <m.m.z.m12363@gmail.com>
2019-10-20 16:18:57 +0330

    add send_voice method

--- gapbot/client/methods/messages/__init__.py
+++ gapbot/client/methods/messages/__init__.py
@@ -2,7 +2,8 @@ from .send_text import SendText
 from .send_image import SendImage
 from .send_audio import SendAudio
 from .send_video import SendVideo
+from .send_voice import SendVoice
 
 
-class Messages(SendText, SendImage, SendAudio, SendVideo):
+class Messages(SendText, SendImage, SendAudio, SendVideo, SendVoice):
     pass
---
+++ gapbot/client/methods/messages/send_voice.py
@@ -0,0 +1,17 @@
+from gapbot.ext import BaseClient
+from json import dumps
+
+
+class SendVoice(BaseClient):
+    def send_voice(self, chat_id, path, reply_keyboard=None, inline_keyboard=None, reply_form=None):
+        return self._send(
+            method='sendMessage',
+            data={
+                'chat_id': chat_id,
+                'type': 'voice',
+                'data': dumps(self.upload(chat_id=chat_id, file_type='voice', path=path).json()),
+                'reply_keyboard': dumps(reply_keyboard) if reply_keyboard else None,
+                'inline_keyboard': dumps(inline_keyboard) if inline_keyboard else None,
+                'form': dumps(reply_form) if reply_form else None,
+            }
+        )
