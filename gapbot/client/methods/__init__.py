5498efd4f7556715830ac4caaebe8c46a3240130
MrMahdi313 <m.m.z.m12363@gmail.com>
2019-10-20 18:36:03 +0330

    add "Callbacks" category to methods

--- gapbot/client/methods/__init__.py
+++ gapbot/client/methods/__init__.py
@@ -1,7 +1,8 @@
 from .decorators import Decorators
 from .messages import Messages
 from .extra import Extra
+from .callbacks import Callbacks
 
 
-class Methods(Decorators, Messages, Extra):
+class Methods(Decorators, Messages, Extra, Callbacks):
     pass
---
+++ gapbot/client/methods/callbacks/__init__.py
@@ -0,0 +1,2 @@
+class Callbacks:
+    pass
