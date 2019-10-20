eb3ca2e3447af46e5c41e5fde21196f47e8873c4
MrMahdi313 <m.m.z.m12363@gmail.com>
2019-10-21 02:09:24 +0330

    add "Payment" category to methods

--- gapbot/client/methods/__init__.py
+++ gapbot/client/methods/__init__.py
@@ -2,7 +2,8 @@ from .decorators import Decorators
 from .messages import Messages
 from .extra import Extra
 from .callbacks import Callbacks
+from .payment import Payment
 
 
-class Methods(Decorators, Messages, Extra, Callbacks):
+class Methods(Decorators, Messages, Extra, Callbacks, Payment):
     pass
