e8c1614056e805ad2a0f94a557cc580c4ea9a64d
MrMahdi313 <m.m.z.m12363@gmail.com>
2019-10-20 10:17:22 +0330

    add send_audio method

--- gapbot/client/methods/messages/__init__.py
+++ gapbot/client/methods/messages/__init__.py
@@ -1,6 +1,7 @@
 from .send_text import SendText
 from .send_image import SendImage
+from .send_audio import SendAudio
 
 
-class Messages(SendText, SendImage):
+class Messages(SendText, SendImage, SendAudio):
     pass
---
+++ gapbot/client/methods/messages/send_audio.py
@@ -0,0 +1,17 @@
+from gapbot.ext import BaseClient
+from json import dumps
+
+
+class SendAudio(BaseClient):
+    def send_audio(self, chat_id, path, reply_keyboard=None, inline_keyboard=None, reply_form=None):
+        return self._send(
+            method='sendMessage',
+            data={
+                'chat_id': chat_id,
+                'type': 'audio',
+                'data': dumps(self.upload(chat_id=chat_id, file_type='audio', path=path).json()),
+                'reply_keyboard': dumps(reply_keyboard) if reply_keyboard else None,
+                'inline_keyboard': dumps(inline_keyboard) if inline_keyboard else None,
+                'form': dumps(reply_form) if reply_form else None,
+            }
+        )
