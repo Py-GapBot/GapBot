8c6962bd7274a0e8d6b6e80ef6b5e5bcc64d6c30
MrMahdi313 <m.m.z.m12363@gmail.com>
2019-10-20 10:16:23 +0330

    add (keyboard, form, inline) to send_image method

--- gapbot/client/methods/messages/send_image.py
+++ gapbot/client/methods/messages/send_image.py
@@ -3,12 +3,15 @@ from json import dumps
 
 
 class SendImage(BaseClient):
-    def send_image(self, chat_id, path):
+    def send_image(self, chat_id, path, reply_keyboard=None, inline_keyboard=None, reply_form=None):
         return self._send(
             method='sendMessage',
             data={
                 'chat_id': chat_id,
                 'type': 'image',
                 'data': dumps(self.upload(chat_id=chat_id, file_type='image', path=path).json()),
+                'reply_keyboard': dumps(reply_keyboard) if reply_keyboard else None,
+                'inline_keyboard': dumps(inline_keyboard) if inline_keyboard else None,
+                'form': dumps(reply_form) if reply_form else None,
             }
         )
