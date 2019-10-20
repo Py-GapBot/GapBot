51b2fd7160c0ddb085f04e36b3085db036386238
MrMahdi313 <m.m.z.m12363@gmail.com>
2019-10-20 10:22:35 +0330

    add send_video method

--- gapbot/client/methods/messages/__init__.py
+++ gapbot/client/methods/messages/__init__.py
@@ -1,7 +1,8 @@
 from .send_text import SendText
 from .send_image import SendImage
 from .send_audio import SendAudio
+from .send_video import SendVideo
 
 
-class Messages(SendText, SendImage, SendAudio):
+class Messages(SendText, SendImage, SendAudio, SendVideo):
     pass
