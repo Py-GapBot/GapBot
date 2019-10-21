cce04fc09192a98e2910770cfa9ec5cf7f0cb52e
MrMahdi313 <m.m.z.m12363@gmail.com>
2019-10-22 00:41:21 +0330

    add "game" category to methods

--- gapbot/client/methods/__init__.py
+++ gapbot/client/methods/__init__.py
@@ -3,7 +3,8 @@ from .messages import Messages
 from .extra import Extra
 from .callbacks import Callbacks
 from .payment import Payment
+from .game import Game
 
 
-class Methods(Decorators, Messages, Extra, Callbacks, Payment):
+class Methods(Decorators, Messages, Extra, Callbacks, Payment, Game):
     pass
