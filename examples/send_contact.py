0f568384fd83bcf3f9ac4f0db0bfee54a00197df
MrMahdi313 <m.m.z.m12363@gmail.com>
2019-10-22 03:01:47 +0330

    add "send_contact" example

---
+++ examples/send_contact.py
@@ -0,0 +1,15 @@
+from gapbot import Gap  # import library;
+from platform import system
+
+app = Gap(bot_token='your_bot_token')  # instance of Main Class;
+# if you need use bot parameters you must set bot_token;
+
+if __name__ == '__main__':  # If you are running this module as the main program;
+    target_id = 000000  # whose to be send text to him;
+    app.send_contact(
+        chat_id=target_id,
+        name='Gap',
+        phone='+989123456789'
+    )
+
+# https://developer.gap.im/doc/botplatform#method-send-contact
