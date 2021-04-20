045a6200b1a02a810d811bbcd9315e68a200a86e
MMahdiZojajiM <m.m.z.m12363@gmail.com>
2021-04-20 17:18:16 +0430

    Add ability to using send_file method with specific file_name parameter

--- gapbot/client/methods/messages/send_file.py
+++ gapbot/client/methods/messages/send_file.py
@@ -3,13 +3,13 @@ from json import dumps
 
 
 class SendFile(BaseClient):
-    def send_file(self, chat_id, path, caption=None, reply_keyboard=None, inline_keyboard=None, reply_form=None):
+    def send_file(self, chat_id, path, caption=None, file_name=None, reply_keyboard=None, inline_keyboard=None, reply_form=None):
         return self._send(
             method='sendMessage',
             data={
                 'chat_id': chat_id,
                 'type': 'file',
-                'data': self.upload(chat_id=chat_id, file_type='file', path=path, desc=caption),
+                'data': self.upload(chat_id=chat_id, file_type='file', path=path, file_name=file_name, desc=caption),
                 'reply_keyboard': dumps(reply_keyboard) if reply_keyboard else None,
                 'inline_keyboard': dumps(inline_keyboard) if inline_keyboard else None,
                 'form': dumps(reply_form) if reply_form else None,
